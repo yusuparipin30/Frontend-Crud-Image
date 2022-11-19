a76fc3bea89c1641f819f526d5cf3185bc6a6d46
yusuparipin30 <yusuparipin30@gmail.com>
2022-11-20 05:58:45 +0700

    all correction crud

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+
 function App() {
   return (
-    <div>
-      <h2>Lorem ipsum dolor sit amet.</h2>
-    </div>
+  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductList/>}/>
+          <Route path="add" element={<AddProduct/>}/>
+          <Route path="edit/:id" element={<EditProduct/>}/>
+        </Routes>
+      </BrowserRouter>
+    
   );
 }
 
